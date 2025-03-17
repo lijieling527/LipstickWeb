fb8d0efe41ef50592254dbd7eef100ff74500d6b
lijieling527 <qoo40328@gmail.com>
2025-03-17 14:06:23 +0800

    0317-8

--- manager.py
+++ manager.py
@@ -1,3 +1,18 @@
-from Lipstick_System import server
+import os
+from flask import Flask
+from flask_mail import Mail
+from flask_bcrypt import Bcrypt
+from gevent import pywsgi
 
-server.serve_forever()
+app = Flask(__name__)
+# app.config.from_object(Config)
+
+# 初始化 Flask 扩展
+mail = Mail(app)
+bcrypt = Bcrypt(app)
+
+if __name__ == "__main__":
+    # 让 Render 监听正确的端口
+    port = int(os.environ.get("PORT", 10000))
+    server = pywsgi.WSGIServer(("0.0.0.0", port), app)
+    server.serve_forever()
